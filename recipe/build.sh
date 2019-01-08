5dd7597b372e2e1b53511be383cdc8b0eea991c5
Mark Harfouche <mark.harfouche@gmail.com>
2019-01-07 21:11:40 -0500

    Use multiple CPUs for the build

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,7 @@ export OPENBLAS_NUM_THREADS=1
 export LIBRARY_PATH="${PREFIX}/lib"
 export C_INCLUDE_PATH="${PREFIX}/include"
 export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+export NPY_NUM_BUILD_JOBS=20
 
 # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ $(uname) == 'Darwin' ]]; then
